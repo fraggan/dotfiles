3b302dc977e86ada4fc395c9910768fa7e0d5964
Fredrik <3606827+fraggan@users.noreply.github.com>
2023-08-12 11:55:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ done
  if hash vim-addon  2>/dev/null; then
          echo "vim-addon (vim-scripts)  installed"
      else
-         echo "vim-addon (vim-scripts) not installed, running 'sudo apt update; sudo apt install bc'"
-         sudo apt update; sudo apt install vim-scripts
+         echo "vim-addon (vim-scripts) not installed, running 'sudo apt update && sudo apt install bc'"
+         sudo apt update && sudo apt install vim-scripts -y
 fi
 
 echo "Installed"
